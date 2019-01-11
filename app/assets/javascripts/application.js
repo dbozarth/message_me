99855b4a7405952dee546fa3938339f0febde21e
dbozarth <don@fuelcnc.com>
2019-01-11 05:43:11 -0600

    UI Start

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery
-//= require jquery-ujs
-//= require popper
-//= require bootstrap
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown').dropdown();
+})
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'chatrooms#index'
   resources :sessions
   resources :chatrooms
+  get 'login', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
